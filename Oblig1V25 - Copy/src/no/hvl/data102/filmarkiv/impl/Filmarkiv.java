c6c4787e517160a4673a7919448b5cba391deffc
Krus-hvl <186463@stud.hvl.no>
2025-01-31 21:17:20 +0100

    Funker dette da?

--- Oblig1V25 - Copy/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ Oblig1V25 - Copy/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -20,6 +20,76 @@ public class Filmarkiv implements FilmarkivADT {
 	}
 	
 	@Override
-	
+	public void leggTilFilm(Film nyFilm) {
+        if (antall == filmer.length) {
+            utvid();
+        }
+        filmer[antall++] = nyFilm;
+    }
+
+    @Override
+    public Film finnFilm(int nr) {
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getFilmNr() == nr) {
+                return filmer[i];
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean slettFilm(int filmnr) {
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getFilmNr() == filmnr) {
+                filmer[i] = filmer[antall - 1];
+                filmer[antall - 1] = null;
+                antall--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+        Film[] treff = new Film[antall];
+        int teller = 0;
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getTittel().toLowerCase().contains(delstreng.toLowerCase())) {
+                treff[teller++] = filmer[i];
+            }
+        }
+        return trimTab(treff, teller);
+    }
+
+    private Film[] trimTab(Film[] tab, int n) {
+        Film[] nytab = new Film[n];
+        for (int i = 0; i < n; i++) {
+            nytab[i] = tab[i];
+        }
+        return nytab;
+    }
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        int teller = 0;
+        for (int i = 0; i < antall; i++) {
+            if (filmer[i].getSjanger() == sjanger) {
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    @Override
+    public int antall() {
+        return antall;
+    }
+
+	@Override
+	public Film[] soekProdusent(String delstreng) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
